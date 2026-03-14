b9c601f4e118f3ca2f9cd1459ac3d7f1cc6a995b
Kibet Bitok <kibetarapsum@gmail.com>
2026-03-14 15:41:50 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,17 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let command = input.trim();
+
+    match command {
+        _ => {
+            eprintln!("{command}: command not found");
+        }
+    }
 }
